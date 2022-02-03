9fae51ee69fdc5fb81f6c1c95fbb7eec6f4c64d4
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-02-03 19:12:30 +0200

    +5 задание

--- task_5/task_5.cpp
+++ task_5/task_5.cpp
@@ -4,7 +4,6 @@
 поочередно (например, A[0]=C[0], B[0]=C[1], A[1]=C[2],
 B[1]=C[3] и т.д.).
 */
-// НЕ ГОТОВО
 
 #include <iostream>
 using std::cout;
@@ -16,19 +15,46 @@ int main()
 	setlocale(LC_ALL, ""); // работает только с потоком вывода
 	srand(time(0));
 
-	int min{ 0 };
-	int max{ 100 };
-	const int arr_1_Size{ 10 };
+	int min{ -9 };
+	int max{ 9 };
+	const int a_Size{ 10 };
+	const int b_Size{ 5 };
+	const int c_Size{ 5 };
 
-	int arr_1[arr_1_Size]{ 0 };
+	int a[a_Size]{ 0 };
+	int b[b_Size]{ 0 };
+	int c[c_Size]{ 0 };
 
-	for (int i = 0; i < arr_1_Size; i++) // заполняем 1 массив и выводим
+	for (int i = 0; i < a_Size; i++) // заполняем a массив и выводим
 	{
-		arr_1[i] = rand() % (max - min + 1) + min;
-		cout << arr_1[i] << ' ';
+		a[i] = rand() % (max - min + 1) + min;
+		cout << a[i] << ' ';
 	}
 	cout << endl;
 
+	int j = 0;
+	for (int i = 0; i < a_Size; i++) // копируем элементы одного массива размером 10 в 2 массива размером 5 элементов поочередно
+	{
+		for (; j < b_Size; )
+		{
+			b[j] = a[i];
+			i++;
+			c[j] = a[i];
+			j++;
+			break;
+		}
+	}
+
+	for (int i = 0; i < b_Size; i++) // выводим b
+	{
+		cout << b[i] << ' ';
+	}
+	cout << endl;
+	for (int i = 0; i < c_Size; i++) // выводим c
+	{
+		cout << c[i] << ' ';
+	}
+	cout << endl;
 
 	return 0;
 }
