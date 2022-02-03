40c5064423e39f09b26c518cce2ca14a28ab382d
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-02-03 17:31:00 +0200

    +3 задание

--- task_3/task_3.cpp
+++ task_3/task_3.cpp
@@ -4,7 +4,6 @@ C из 10 элементов, в который копируются элеме�
 второго массива поочередно (например, С[0]=A[0], С[1]=B[0],
 C[2]=A[1], C[3]=B[1] и т.д.).
 */
-// НЕ ГОТОВО
 
 #include <iostream>
 using std::cout;
@@ -18,17 +17,39 @@ int main()
 
 	int min{ 0 };
 	int max{ 100 };
-	const int arr_1_Size{ 10 };
+	const int a_Size{ 5 };
+	const int b_Size{ 5 };
+	const int c_Size{ 10 };
 
-	int arr_1[arr_1_Size]{ 0 };
+	int a[a_Size]{ 0 };
+	int b[b_Size]{ 0 };
+	int c[c_Size]{ 0 };
 
-	for (int i = 0; i < arr_1_Size; i++) // заполняем 1 массив и выводим
+	for (int i = 0; i < a_Size; i++) // заполняем a массив и выводим
 	{
-		arr_1[i] = rand() % (max - min + 1) + min;
-		cout << arr_1[i] << ' ';
+		a[i] = rand() % (max - min + 1) + min;
+		cout << a[i] << ' ';
+	}
+	cout << endl;
+	for (int i = 0; i < b_Size; i++) // заполняем b массив и выводим
+	{
+		b[i] = rand() % (max - min + 1) + min;
+		cout << b[i] << ' ';
 	}
 	cout << endl;
 
+	for (int i = 0, j = 0; i < c_Size; i++, j++)
+	{
+		c[i] = a[j];
+		i++;
+		c[i] = b[j];
+	}
+
+	for (int i = 0; i < c_Size; i++) // выводим c
+	{
+		cout << c[i] << ' ';
+	}
+	cout << endl;
 
 	return 0;
 }
